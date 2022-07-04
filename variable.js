19424e83725fabd68ced334afad0a1303a38dd87
mariadc21! <mariferdc2002@gmail.com>
2022-07-03 21:44:52 -0400

    very easy and easy challenge

---
+++ variable.js
@@ -0,0 +1,23 @@
+// Very Easy
+
+var num1= 6;
+var num2= 2;
+var num3= num1 - num2;
+console.log(` The difference between ${num1} and ${num2} is ${num3}`)
+
+// Easy 
+var person1="Thomas";
+var person2= "John";
+
+if(person1.length > person2.length){
+    console.log(`the name ${person1} is longer than ${person2} by ${person1.length - person2.length} characters `)
+} else if (person2.length > person1.length){
+    console.log(`the name ${person2} is longer than ${person1} by ${person2.length - person1.length} characters `)
+}
+// MEDIUM: Write a program to tell if someone is shouting (typing in all caps),
+//  whispering (typing in all lowercase), or neither. 
+//  Use prompt to get user input, and then console log whether the user
+//   was shouting, whispering, or talking normally.
+// HARD: Create 4 math functions, one called add() that adds 2 numbers, one called subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
+
+
